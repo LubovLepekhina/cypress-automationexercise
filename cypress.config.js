7946f2403f335417c25cc3c1cad086ea7719cb08
Tatiana BM <burykina.tatiana@gmail.com>
2024-11-09 19:34:20 +0100

    add sigh up login test

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   watchForFileChanges: false,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
