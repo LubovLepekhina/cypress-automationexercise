a246e768c0e10644a7b50017be73bf2c071fb66c
LubovLepekhina <lubov.lepekhina@gmail.com>
2024-11-14 13:57:30 +0100

    add test create new user

--- cypress/e2e/tests_lu.cy.js
+++ cypress/e2e/tests_lu.cy.js
@@ -1,16 +1,73 @@
 /// <reference types='cypress' />
-import {newUser} from '../fixtures/signUpLogin.json'
+
+function deleteAcc() {
+  cy.get('a[href$="/delete_account"]').click()
+  cy.get('h2').contains('Account Deleted!').should('be.visible')
+}
+
+describe('1. create new user', () => {
+  before(() => {
+    cy.visit('/')
+    cy.fixture('usersData.json').as('user')
+  })
+  it('create a new user with all required fields', () => {
+    cy.url().should('include', 'https://automationexercise.com')
+    cy.title().should('eq', 'Automation Exercise')
+    cy.get("a[href$='login']").click()
+    cy.get('h2').contains('New User Signup!').should('be.visible')
+    cy.get('@user').then((user) => {
+      cy.get("input[data-qa='signup-name']").type(user.name)
+      cy.get("input[data-qa='signup-email']").type(user.email)
+    })
+    cy.get("button[data-qa='signup-button']").click()
+    cy.url().should('include', '/signup')
+    cy.get('h2').contains('Enter Account Information').should('be.visible')
+
+    cy.get('@user').then((user) => {
+      if (user.gender === 'female') {
+        cy.get("input[id='id_gender2']").check()
+      } else if (user.gender === 'male') {
+        cy.get("input[id='id_gender1']").check()
+      }
+      cy.get('input#name').should('have.value', user.name)
+      cy.get('input#email').should('have.value', user.email)
+
+      cy.get('.required input').each(($el) => {
+        let sel = $el.attr('id')
+        cy.log(sel)
+        if ($el.attr('value')) {
+          return
+        } 
+        if (user[sel]) {
+          cy.wrap($el).clear().type(user[sel])
+        }
+      })
+      cy.get('select#country').select(user.country).should('have.value', 'United States')
+    })
+    cy.get('button').contains('Create Account').click()
+
+    cy.url().should('include', '/account_created')
+    cy.get('h2').contains('Account Created!').should('be.visible')
+    cy.get('a').contains('Continue').click()
+
+    cy.get('@user').then((user) => {
+      cy.get('a').contains(`Logged in as ${user.name}`).should('be.visible')
+    })
+  })
+  // after(() => {
+  //   deleteAcc()
+  // })
+})
 
 describe('template spec', () => {
   beforeEach(() => {
     // 1. Launch browser
     // 2. Navigate to url 'http://automationexercise.com'
     cy.visit('/')
+    cy.fixture('usersData.json').as('user')
+
   })
-  it.only('2. Login User with correct email and password', () => {
-    let email = 'nikole@gmail.com'
-    let password = 'nikole'
-    let username = 'Nikole'
+  it('2. Login User with correct email and password', () => {
     // 3. Verify that home page is visible successfully
     cy.url().should('include', 'https://automationexercise.com')
     cy.title().should('eq', 'Automation Exercise')
@@ -22,21 +79,17 @@ describe('template spec', () => {
     cy.get('h2').contains('Login to your account').should('be.visible')
 
     // 6. Enter correct email address and password
-    cy.get("input[data-qa='login-email']").type(email)
-    cy.get("input[data-qa='login-password']").type(password)
-
-    // 7. Click 'login' button
-    cy.get('button').contains('Login').click()
-
-    // 8. Verify that 'Logged in as username' is visible
-    cy.get('a').contains(`Logged in as ${username}`).should('be.visible')
-
-    // 9. Click 'Delete Account' button
-    cy.get('a[href$="/delete_account"]').click()
-
-    // 10. Verify that 'ACCOUNT DELETED!' is visible
-    cy.get('h2').contains('Account Deleted!').should('be.visible')
+    cy.get('@user').then((user) => {
+      cy.get("input[data-qa='login-email']").type(user.email)
+      cy.get("input[data-qa='login-password']").type(user.password)
+          // 7. Click 'login' button
+      cy.get('button').contains('Login').click()
+          // 8. Verify that 'Logged in as username' is visible
+      cy.get('a').contains(`Logged in as ${user.name}`).should('be.visible')
+    })
+    deleteAcc()
   })
+
   it('3. Login User with incorrect email and password', () => {
     // 3. Verify that home page is visible successfully
     cy.url().should('include', 'https://automationexercise.com')
