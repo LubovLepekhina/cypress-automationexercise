285716d4c6f8119b67ba87d95419c50419274bb5
TatianaBM <130609785+TatianaBM@users.noreply.github.com>
2025-01-07 10:59:19 +0100

    add the same product to cart (#17)

--- cypress/pageObjects/headerPage.js
+++ cypress/pageObjects/headerPage.js
@@ -5,7 +5,7 @@ class Header {
     getLoggedinAs = () => cy.contains('.navbar-nav li', 'Logged in as')
     getDeleteAccountLink = () => cy.get('a[href="/delete_account"]')
     getLogOutLink = () => cy.get('a[href="/logout"]')
-    getProductsLink = () => cy.get('a[href="/products"]')
+    getProductsLink = () => cy.get('.navbar-nav a[href="/products"]')
     getCartLink = () => cy.get('a[href="/view_cart"]').first()
 
     clickSighUpLoginLink() {
