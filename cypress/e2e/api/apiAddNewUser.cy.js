dd80ff082e3e53f75be653d502668ab7b4de51dd
Tatiana BM <burykina.tatiana@gmail.com>
2025-01-15 14:21:47 +0100

    error when creating new user with existing data

--- cypress/e2e/api/apiAddNewUser.cy.js
+++ cypress/e2e/api/apiAddNewUser.cy.js
@@ -1,13 +1,18 @@
 import validateSchema from 'cypress-ajv-schema-validator'
 chai.use(require('chai-json-schema'))
 import spok from 'cy-spok'
+import { faker } from "@faker-js/faker"
 
 import genData from '../../fixtures/genData'
 import schemas from '../../fixtures/api/schemas.json'
+import errorMessage from '../../fixtures/api/errorMessages.json'
+
+import { createNewUserApi } from '../../support/utils'
+import {ENDPOINTS} from '../../support/endpoints'
 
 describe('create new user', () => {
 
-    let newUser
+let newUser
     beforeEach(() => {
         newUser = genData.newUser()
     })
@@ -31,6 +36,28 @@ describe('create new user', () => {
             message: "User created!",
             responseCode: 201
         }))
+    })
+
+    it('errors when creating a user with existing data set', () => {
+        cy.log('precondition')
+        createNewUserApi(ENDPOINTS.API.createAccount, newUser).its('status').should('eq', 200)
+        cy.log('create user with the same data set')
+        createNewUserApi(ENDPOINTS.API.createAccount, newUser).then(resp => {
+            expect(resp.status).to.equal(200)
+            return JSON.parse(resp.body)
+        }).should(spok({
+            responseCode: 400,
+            message: errorMessage.createNewUser[0]
+            }))
+    })
+    
+    it('checks json schema creating a user with existing data set', () => {
+        cy.log('precondition')
+        createNewUserApi(ENDPOINTS.API.createAccount, newUser).its('status').should('eq', 200)
+        cy.log('create user with the same data set')
+        createNewUserApi(ENDPOINTS.API.createAccount, newUser).then(resp => {
+            expect(JSON.parse(resp.body)).to.be.jsonSchema(schemas.emailExist)
+        })
     })
 
 })
--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -10,3 +10,34 @@ export function calculateBrandQuantity(products) {
     })
     return eachBrandQuantity
 }
+
+export function createNewUserApi(endpoint, userdata) {
+    return cy.request({
+        method: 'POST',
+        url: endpoint,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        failOnStatusCode: false,
+        body: {
+            title: userdata.gender,
+            name: userdata.name,
+            email: userdata.emailAddress,
+            password: userdata.password,
+            birth_date: userdata.birthDate.dateOfBirth,
+            birth_month: userdata.birthDate.monthOfBirth,
+            birth_year: userdata.birthDate.yearOfBirth,
+            firstname: userdata.firstName,
+            lastname: userdata.lastName,
+            company: userdata.company,
+            address1: userdata.address,
+            address2: userdata.address,
+            country: userdata.country,
+            zipcode: userdata.zipCode,
+            state: userdata.state,
+            city: userdata.city,
+            mobile_number: userdata.mobileNumber,
+        }
+    })
+
+}
