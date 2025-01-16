a97080d059c202e3229aa3e4ff641858fd35ab5c
LubovLepekhina <113927670+LubovLepekhina@users.noreply.github.com>
2025-01-16 19:39:51 +0100

    add checkoutPageData.json (#32)

--- cypress/e2e/UI POM/checkCartBeforeOrder.cy.js
+++ cypress/e2e/UI POM/checkCartBeforeOrder.cy.js
@@ -7,6 +7,7 @@ import LoginPage from '../../pageObjects/loginPage'
 import genData from '../../fixtures/genData'
 import CheckoutPage from '../../pageObjects/checkoutPage'
 import { ENDPOINTS } from "../../support/endpoints"
+import checkoutData from '../../fixtures/checkoutPageData.json'
 
 const productsPage = new ProductsPage()
 const headerPage = new Header()
@@ -36,27 +37,31 @@ describe('check the cart before ordering', () => {
         cy.apiDeleteUserAccount(newUser.emailAddress, newUser.password)
     })
 
-    it ('verify the delivery address matches the address at registration', () => {
-        productsPage.clickProductsLink()
-            .addRandomProductToCart()
-        cy.wait('@addedToCart')
-        headerPage.clickModalViewCartLink()
-        cartPage.clickProceedToCheckoutBtn()
+    checkoutData.addressTypes.forEach((addressType, i) => {
+        it (`verify the ${addressType} address matches the address at registration`, () => {
+            productsPage.clickProductsLink()
+                .addRandomProductToCart()
+            cy.wait('@addedToCart')
+            headerPage.clickModalViewCartLink()
+            cartPage.clickProceedToCheckoutBtn()
 
-        cy.url().should('contain', 'checkout')
-        
-        checkoutPage.getDeliveryAddressBox().invoke('text').then((text) => {
-            const cleanedText = text.replace(/\s/g, ' ').trim()
-            expect(cleanedText).contains(newUser.firstName)
-            expect(cleanedText).contains(newUser.lastName)
-            expect(cleanedText).contains(newUser.gender)
-            expect(cleanedText).contains(newUser.company)
-            expect(cleanedText).contains(newUser.address)
-            expect(cleanedText).contains(newUser.country)
-            expect(cleanedText).contains(newUser.state)
-            expect(cleanedText).contains(newUser.city)
-            expect(cleanedText).contains(newUser.zipCode)
-            expect(cleanedText).contains(newUser.mobileNumber)
-        })        
+            cy.url().should('contain', ENDPOINTS.UI.CHECKOUT)
+
+            let methodName = `get${addressType}AddressBox` 
+            checkoutPage[methodName]().invoke('text').then((text) => {
+                const cleanedText = text.replace(/\s/g, ' ').trim()
+                expect(cleanedText).contains(checkoutData.boxesNames[i])
+                expect(cleanedText).contains(newUser.firstName)
+                expect(cleanedText).contains(newUser.lastName)
+                expect(cleanedText).contains(newUser.gender)
+                expect(cleanedText).contains(newUser.company)
+                expect(cleanedText).contains(newUser.address)
+                expect(cleanedText).contains(newUser.country)
+                expect(cleanedText).contains(newUser.state)
+                expect(cleanedText).contains(newUser.city)
+                expect(cleanedText).contains(newUser.zipCode)
+                expect(cleanedText).contains(newUser.mobileNumber)
+            })
+        })
     })
 })
